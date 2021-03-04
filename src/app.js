38ef4ae3c61f53a7ae72f1e3332f068c9a52b590
MrGustave184 <rafaeldavid184@gmail.com>
2021-03-04 12:37:02 -0400

    refactor and add port

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const { registerUser, getUsers } = require('./core/user');
+
 const app = require("express")();
 const cors = require("cors")
-
 app.use(cors())
 
 const httpServer = require("http").createServer(app);
@@ -20,13 +20,13 @@ const options = {
 		methods: ["GET", "POST"]
 	}
 };
-const io = require("socket.io")(httpServer, options);
 
+const io = require("socket.io")(httpServer, options);
 
 io.on("connection", socket => { 
     socket.on('newUser', ({ client, project }) => {
 
-        let room = `${client}_${project}`;
+        const room = `${client}_${project}`;
 
         const user = registerUser({
             id: socket.id,
@@ -35,21 +35,19 @@ io.on("connection", socket => {
             room
         });
 
-        // console.log(user);
-
         socket.join(room);
 
-        // pass room to filter!
         const users = getUsers(room);
 
         socket.emit('message', users);
 
-        // Broadcast (everyone but the new client)
+        // Broadcast (everyone but the new client) by room
         socket.broadcast.to(room).emit('message', user);
     });
-
 });
 
-httpServer.listen(3000, () => {
-	console.log('Listening');
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(PORT, () => {
+	console.log(`server running on port: ${$PORT}`);
 });
